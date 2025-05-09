95a0d418dcb730f6395df386e62256afa857a170
UncleH25 <oneilljarryd@gmail.com>
2025-05-09 18:58:01 +0200

    Declared initial global const variables, and one for the api key.

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+//Simple Weather Dashboard
+
+// Global Variables
+const locationForm = document.querySelector(".location-info");
+const locationInput = document.querySelector(".location-input");
+const weatherSection = document.querySelector(".weather-info");
+//API Key
+const apiKey = config.apiKey;
